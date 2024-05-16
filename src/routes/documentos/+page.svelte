2ac0f4a9e7d5f0a9461417022caaa3ef17abdca7
lucas210898.lao <lucas210898.lao@gmail.com>
2024-05-16 21:17:52 +0000

    fix(app): :ambulance: removida referencias a tapeditor

--- src/routes/documentos/+page.svelte
+++ src/routes/documentos/+page.svelte
@@ -1,28 +1,6 @@
 <script lang="ts">
-	import {Section} from '$lib/components/section';
+	import { Section } from '$lib/components/section';
 	import { Card, CardHeader, CardTitle } from '$lib/components/ui/card';
-	import CardFooter from '$lib/components/ui/card/card-footer.svelte';
-	import { Editor, EditorCore } from '$lib/tapeditor';
-	import BubbleMenu from '$lib/tapeditor/BubbleMenu.svelte';
-	import MenuBar from '$lib/tapeditor/MenuBar.svelte';
-	import createEditor from '$lib/tapeditor/createEditor';
-	import { cn } from '$lib/utils';
-	import StarterKit from '@tiptap/starter-kit';
-	import type { Readable } from 'svelte/store';
-
-	let editor: Readable<EditorCore>;
-
-	$effect(() => {
-		editor = createEditor({
-			extensions: [StarterKit],
-			content: `<p>Hello World!</p>`,
-			editorProps: {
-				attributes: {
-					class: cn(`outline-none prose !text-muted-foreground`)
-				}
-			}
-		});
-	});
 </script>
 
 <Section>
@@ -30,17 +8,5 @@
 		<CardHeader>
 			<CardTitle>Edite o texto</CardTitle>
 		</CardHeader>
-		<Editor editor={$editor}></Editor>
-		{#if $editor}
-			<BubbleMenu editor={$editor}>
-				<MenuBar editor={$editor} />
-			</BubbleMenu>
-		{/if}
-
-		<CardFooter>
-			{#if $editor}
-				<MenuBar editor={$editor} />
-			{/if}
-		</CardFooter>
 	</Card>
 </Section>
